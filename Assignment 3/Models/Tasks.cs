a13a76982fdd492f3be37a65efe06c397a9a8a9d
maramkhaledd <155253791+maramkhaledd@users.noreply.github.com>
2026-08-16 20:22:35 +0300

    Add files via upload

---
+++ Assignment 3/Models/Tasks.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment_3.Models
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+   
+    }
+}
